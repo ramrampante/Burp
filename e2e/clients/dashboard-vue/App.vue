7a37ae79e33b7e5b08b21649dae89b0dfcce0f2c
ramrampante <ramonarampanteramonarampantera@gmail.com>
2024-10-03 10:20:01 +0300

    Optimizing performance of e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :Burp="Burp" />
+</template>
+
+<script>
+import Vue from 'vue'
+import Burp from '@Burp/core'
+import { Dashboard } from '@Burp/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    Burp: () => new Burp(),
+  },
+}
+</script>
+
+<style src="@Burp/core/dist/style.css"></style>
+<style src="@Burp/dashboard/dist/style.css"></style>
